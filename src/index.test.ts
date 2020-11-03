ed513694282c86c2b4a1296a7e89df6f52343847
dai <chamestica@gmail.com>
2020-11-04 00:40:54 +0900

    Export non-default values too

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,11 @@
-import ViewportExtra from './index'
+import ViewportExtra, { version } from './index'
 
 describe('about src/index.ts', () => {
-  test('exported class contains version property', () => {
+  test('whether to export version property', () => {
+    expect(typeof version === 'string').toBe(true)
+  })
+
+  test('whether to export class containing version property', () => {
     expect(typeof ViewportExtra.version === 'string').toBe(true)
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import * as packageJson from '../package.json'
 
+export const version = packageJson.version
+
 const ViewportExtra = {
   version: packageJson.version
 }
-
 export default ViewportExtra
