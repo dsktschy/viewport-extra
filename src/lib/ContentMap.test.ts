bb440c7a2681ecae8a030693f40ce173ce2ac2b4
dai <chamestica@gmail.com>
2020-11-06 01:33:37 +0900

    Validate min-width and max-width

--- src/lib/ContentMap.test.ts
+++ src/lib/ContentMap.test.ts
@@ -1,4 +1,7 @@
-import { stringifyContentMap } from './ContentMap'
+import {
+  stringifyContentMap,
+  isValidAsViewportExtraContentMap
+} from './ContentMap'
 
 describe('about src/lib/ContentMap.ts', () => {
   test('stringifyContentMap', () => {
@@ -7,5 +10,70 @@ describe('about src/lib/ContentMap.ts', () => {
       'initial-width': '1'
     })
     expect(contentString).toBe('width=device-width,initial-width=1')
+  })
+
+  test('isValidAsViewportExtraContentMap returns true when receiving min-width and max-width, and they are numeric strings and min-width <= max-width', () => {
+    const viewportExtraContentMapValid = isValidAsViewportExtraContentMap({
+      'min-width': '375',
+      'max-width': '414'
+    })
+    expect(viewportExtraContentMapValid).toBe(true)
+  })
+
+  test('isValidAsViewportExtraContentMap returns false when receiving min-width and max-width, but min-width > max-width', () => {
+    const viewportExtraContentMapValid = isValidAsViewportExtraContentMap({
+      'min-width': '414',
+      'max-width': '375'
+    })
+    expect(viewportExtraContentMapValid).toBe(false)
+  })
+
+  test('isValidAsViewportExtraContentMap returns false when receiving min-width and max-width, but min-width is not numeric string', () => {
+    const viewportExtraContentMapValid = isValidAsViewportExtraContentMap({
+      'min-width': 'foobar',
+      'max-width': '414'
+    })
+    expect(viewportExtraContentMapValid).toBe(false)
+  })
+
+  test('isValidAsViewportExtraContentMap returns false when receiving min-width and max-width, but max-width is not numeric string', () => {
+    const viewportExtraContentMapValid = isValidAsViewportExtraContentMap({
+      'min-width': '375',
+      'max-width': 'foobar'
+    })
+    expect(viewportExtraContentMapValid).toBe(false)
+  })
+
+  test('isValidAsViewportExtraContentMap returns true when receiving min-width, and it is numeric string', () => {
+    const viewportExtraContentMapValid = isValidAsViewportExtraContentMap({
+      'min-width': '375'
+    })
+    expect(viewportExtraContentMapValid).toBe(true)
+  })
+
+  test('isValidAsViewportExtraContentMap returns false when receiving min-width, and it is not numeric string', () => {
+    const viewportExtraContentMapValid = isValidAsViewportExtraContentMap({
+      'min-width': 'foobar'
+    })
+    expect(viewportExtraContentMapValid).toBe(false)
+  })
+
+  test('isValidAsViewportExtraContentMap returns true when receiving max-width, and it is numeric string', () => {
+    const viewportExtraContentMapValid = isValidAsViewportExtraContentMap({
+      'max-width': '414'
+    })
+    expect(viewportExtraContentMapValid).toBe(true)
+  })
+
+  test('isValidAsViewportExtraContentMap returns false when receiving max-width, and it is not numeric string', () => {
+    const viewportExtraContentMapValid = isValidAsViewportExtraContentMap({
+      'max-width': 'foobar'
+    })
+    expect(viewportExtraContentMapValid).toBe(false)
+  })
+
+  test('isValidAsViewportExtraContentMap returns false when not receiving both of min-width and max-width', () => {
+    const viewportExtraContentMapValid = isValidAsViewportExtraContentMap({})
+    expect(viewportExtraContentMapValid).toBe(false)
   })
 })
--- src/lib/ContentMap.ts
+++ src/lib/ContentMap.ts
@@ -9,3 +9,23 @@ export const stringifyContentMap = (contentMap: ContentMap): ContentString => {
     .map(([key, value]) => `${key}=${value}`)
     .join(',')
 }
+
+export const isValidAsViewportExtraContentMap = (
+  contentMap: ContentMap
+): boolean => {
+  // Not numeric string
+  const minWidthDefined = contentMap['min-width'] != null
+  const numericMinWidth = +contentMap['min-width']
+  if (minWidthDefined && Number.isNaN(numericMinWidth)) return false
+  const maxWidthDefined = contentMap['max-width'] != null
+  const numericMaxWidth = +contentMap['max-width']
+  if (maxWidthDefined && Number.isNaN(numericMaxWidth)) return false
+
+  // Must be mix-width <= max-width
+  // At least one of mix-width or max-width
+  return minWidthDefined
+    ? maxWidthDefined
+      ? numericMinWidth <= numericMaxWidth
+      : true
+    : maxWidthDefined
+}
