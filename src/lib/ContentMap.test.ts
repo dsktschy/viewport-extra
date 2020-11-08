73d73d7b89201e151894965ab8813fd2c5289259
dai <chamestica@gmail.com>
2020-11-08 23:39:07 +0900

    Create contentMap that options are applied

--- src/lib/ContentMap.test.ts
+++ src/lib/ContentMap.test.ts
@@ -1,76 +1,415 @@
-import { stringify, validateExtraProperties } from './ContentMap'
+import {
+  ContentMap,
+  defaultViewportProps,
+  forcedViewportProps,
+  defaultViewportExtraProps,
+  forcedViewportExtraProps,
+  initializeViewportProps,
+  initializeViewportExtraProps,
+  stringify,
+  createAppliedContentMap
+} from './ContentMap'
+import {
+  NonnumericInitialScaleError,
+  NoOptionsError,
+  NonnumericMinWidthError,
+  NonnumericMaxWidthError,
+  ReversedOptionsError
+} from './Error'
 
 describe('about src/lib/ContentMap.ts', () => {
+  test('initializeViewportProps applys initial props', () => {
+    const viewportContentMap = initializeViewportProps({})
+    expect(viewportContentMap).toStrictEqual({
+      ...defaultViewportProps,
+      ...forcedViewportProps
+    })
+  })
+
+  test('initializeViewportProps overrides forced props and input props overrides default props', () => {
+    const inputViewportContentMap: ContentMap = {
+      ...defaultViewportProps,
+      ...forcedViewportProps
+    }
+    for (const [key, value] of Object.entries(inputViewportContentMap)) {
+      inputViewportContentMap[key] = `${value}foobar`
+    }
+    const expectedViewportContentMap: ContentMap = forcedViewportProps
+    for (const key of Object.keys(defaultViewportProps)) {
+      expectedViewportContentMap[key] = inputViewportContentMap[key]
+    }
+    const viewportContentMap = initializeViewportProps(inputViewportContentMap)
+    expect(viewportContentMap).toStrictEqual(expectedViewportContentMap)
+  })
+
+  test('initializeViewportExtraProps applys initial props', () => {
+    const viewportExtraContentMap = initializeViewportExtraProps({})
+    expect(viewportExtraContentMap).toStrictEqual({
+      ...defaultViewportExtraProps,
+      ...forcedViewportExtraProps
+    })
+  })
+
+  test('initializeViewportExtraProps overrides forced props and input props overrides default props', () => {
+    const inputViewportExtraContentMap: ContentMap = {
+      ...defaultViewportExtraProps,
+      ...forcedViewportExtraProps
+    }
+    for (const [key, value] of Object.entries(inputViewportExtraContentMap)) {
+      inputViewportExtraContentMap[key] = `${value}foobar`
+    }
+    const expectedViewportExtraContentMap: ContentMap = forcedViewportExtraProps
+    for (const key of Object.keys(defaultViewportExtraProps)) {
+      expectedViewportExtraContentMap[key] = inputViewportExtraContentMap[key]
+    }
+    const viewportExtraContentMap = initializeViewportExtraProps(
+      inputViewportExtraContentMap
+    )
+    expect(viewportExtraContentMap).toStrictEqual(
+      expectedViewportExtraContentMap
+    )
+  })
+
+  test('createAppliedContentMap throws error when receiving initial-scale that is not numeric string', () => {
+    const viewportContentMap = {
+      width: 'device-width',
+      'initial-scale': 'foobar'
+    }
+    const viewportExtraContentMap = {
+      'min-width': '375',
+      'max-width': '414'
+    }
+    const documentClientWidth = 390
+    const bindedCreateAppliedContentMap = createAppliedContentMap.bind(
+      null,
+      viewportContentMap,
+      viewportExtraContentMap,
+      documentClientWidth
+    )
+    expect(bindedCreateAppliedContentMap).toThrowError(
+      NonnumericInitialScaleError
+    )
+  })
+
+  test('createAppliedContentMap throws error when not receiving min-width and max-width', () => {
+    const viewportContentMap = {
+      width: 'device-width',
+      'initial-scale': '1'
+    }
+    const viewportExtraContentMap = {}
+    const documentClientWidth = 390
+    const bindedCreateAppliedContentMap = createAppliedContentMap.bind(
+      null,
+      viewportContentMap,
+      viewportExtraContentMap,
+      documentClientWidth
+    )
+    expect(bindedCreateAppliedContentMap).toThrowError(NoOptionsError)
+  })
+
+  test('createAppliedContentMap throws error when receiving min-width that is not numeric string', () => {
+    const viewportContentMap = {
+      width: 'device-width',
+      'initial-scale': '1'
+    }
+    const viewportExtraContentMap = {
+      'min-width': 'foobar',
+      'max-width': '414'
+    }
+    const documentClientWidth = 390
+    const bindedCreateAppliedContentMap = createAppliedContentMap.bind(
+      null,
+      viewportContentMap,
+      viewportExtraContentMap,
+      documentClientWidth
+    )
+    expect(bindedCreateAppliedContentMap).toThrowError(NonnumericMinWidthError)
+  })
+
+  test('createAppliedContentMap throws error when receiving max-width that is not numeric string', () => {
+    const viewportContentMap = {
+      width: 'device-width',
+      'initial-scale': '1'
+    }
+    const viewportExtraContentMap = {
+      'min-width': '375',
+      'max-width': 'foobar'
+    }
+    const documentClientWidth = 390
+    const bindedCreateAppliedContentMap = createAppliedContentMap.bind(
+      null,
+      viewportContentMap,
+      viewportExtraContentMap,
+      documentClientWidth
+    )
+    expect(bindedCreateAppliedContentMap).toThrowError(NonnumericMaxWidthError)
+  })
+
+  test('createAppliedContentMap throws error when receiving max-width that is less than min-width', () => {
+    const viewportContentMap = {
+      width: 'device-width',
+      'initial-scale': '1'
+    }
+    const viewportExtraContentMap = {
+      'min-width': '414',
+      'max-width': '375'
+    }
+    const documentClientWidth = 390
+    const bindedCreateAppliedContentMap = createAppliedContentMap.bind(
+      null,
+      viewportContentMap,
+      viewportExtraContentMap,
+      documentClientWidth
+    )
+    expect(bindedCreateAppliedContentMap).toThrowError(ReversedOptionsError)
+  })
+
+  test('createAppliedContentMap with following params. initial-scale: 1, min-width: 375, documentClientWidth: 374', () => {
+    const initialScale = '1'
+    const minWidth = '375'
+    const viewportContentMap = {
+      width: 'device-width',
+      'initial-scale': initialScale
+    }
+    const viewportExtraContentMap = {
+      'min-width': minWidth
+    }
+    const documentClientWidth = 374
+    const contentMap = createAppliedContentMap(
+      viewportContentMap,
+      viewportExtraContentMap,
+      documentClientWidth
+    )
+    expect(contentMap).toStrictEqual({
+      width: minWidth,
+      'initial-scale': `${(documentClientWidth / +minWidth) * +initialScale}`
+    })
+  })
+
+  test('createAppliedContentMap with following params. initial-scale: 1, min-width: 375, documentClientWidth: 375', () => {
+    const initialScale = '1'
+    const minWidth = '375'
+    const viewportContentMap = {
+      width: 'device-width',
+      'initial-scale': initialScale
+    }
+    const viewportExtraContentMap = {
+      'min-width': minWidth
+    }
+    const documentClientWidth = 375
+    const contentMap = createAppliedContentMap(
+      viewportContentMap,
+      viewportExtraContentMap,
+      documentClientWidth
+    )
+    expect(contentMap).toStrictEqual(viewportContentMap)
+  })
+
+  test('createAppliedContentMap with following params. initial-scale: 1, max-width: 414, documentClientWidth: 414', () => {
+    const initialScale = '1'
+    const maxWidth = '414'
+    const viewportContentMap = {
+      width: 'device-width',
+      'initial-scale': initialScale
+    }
+    const viewportExtraContentMap = {
+      'max-width': maxWidth
+    }
+    const documentClientWidth = 414
+    const contentMap = createAppliedContentMap(
+      viewportContentMap,
+      viewportExtraContentMap,
+      documentClientWidth
+    )
+    expect(contentMap).toStrictEqual(viewportContentMap)
+  })
+
+  test('createAppliedContentMap with following params. initial-scale: 1, max-width: 414, documentClientWidth: 415', () => {
+    const initialScale = '1'
+    const maxWidth = '414'
+    const viewportContentMap = {
+      width: 'device-width',
+      'initial-scale': initialScale
+    }
+    const viewportExtraContentMap = {
+      'max-width': maxWidth
+    }
+    const documentClientWidth = 415
+    const contentMap = createAppliedContentMap(
+      viewportContentMap,
+      viewportExtraContentMap,
+      documentClientWidth
+    )
+    expect(contentMap).toStrictEqual({
+      width: maxWidth,
+      'initial-scale': `${(documentClientWidth / +maxWidth) * +initialScale}`
+    })
+  })
+
+  test('createAppliedContentMap with following params. initial-scale: 1, min-width: 375, max-width: 414, documentClientWidth: 374', () => {
+    const initialScale = '1'
+    const minWidth = '375'
+    const maxWidth = '414'
+    const viewportContentMap = {
+      width: 'device-width',
+      'initial-scale': initialScale
+    }
+    const viewportExtraContentMap = {
+      'min-width': minWidth,
+      'max-width': maxWidth
+    }
+    const documentClientWidth = 374
+    const contentMap = createAppliedContentMap(
+      viewportContentMap,
+      viewportExtraContentMap,
+      documentClientWidth
+    )
+    expect(contentMap).toStrictEqual({
+      width: minWidth,
+      'initial-scale': `${(documentClientWidth / +minWidth) * +initialScale}`
+    })
+  })
+
+  test('createAppliedContentMap with following params. initial-scale: 1, min-width: 375, max-width: 414, documentClientWidth: 375', () => {
+    const initialScale = '1'
+    const minWidth = '375'
+    const maxWidth = '414'
+    const viewportContentMap = {
+      width: 'device-width',
+      'initial-scale': initialScale
+    }
+    const viewportExtraContentMap = {
+      'min-width': minWidth,
+      'max-width': maxWidth
+    }
+    const documentClientWidth = 375
+    const contentMap = createAppliedContentMap(
+      viewportContentMap,
+      viewportExtraContentMap,
+      documentClientWidth
+    )
+    expect(contentMap).toStrictEqual(viewportContentMap)
+  })
+
+  test('createAppliedContentMap with following params. initial-scale: 1, min-width: 375, max-width: 414, documentClientWidth: 414', () => {
+    const initialScale = '1'
+    const minWidth = '375'
+    const maxWidth = '414'
+    const viewportContentMap = {
+      width: 'device-width',
+      'initial-scale': initialScale
+    }
+    const viewportExtraContentMap = {
+      'min-width': minWidth,
+      'max-width': maxWidth
+    }
+    const documentClientWidth = 414
+    const contentMap = createAppliedContentMap(
+      viewportContentMap,
+      viewportExtraContentMap,
+      documentClientWidth
+    )
+    expect(contentMap).toStrictEqual(viewportContentMap)
+  })
+
+  test('createAppliedContentMap with following params. initial-scale: 1, min-width: 375, max-width: 414, documentClientWidth: 415', () => {
+    const initialScale = '1'
+    const minWidth = '375'
+    const maxWidth = '414'
+    const viewportContentMap = {
+      width: 'device-width',
+      'initial-scale': initialScale
+    }
+    const viewportExtraContentMap = {
+      'min-width': minWidth,
+      'max-width': maxWidth
+    }
+    const documentClientWidth = 415
+    const contentMap = createAppliedContentMap(
+      viewportContentMap,
+      viewportExtraContentMap,
+      documentClientWidth
+    )
+    expect(contentMap).toStrictEqual({
+      width: maxWidth,
+      'initial-scale': `${(documentClientWidth / +maxWidth) * +initialScale}`
+    })
+  })
+
+  test('createAppliedContentMap with following params. initial-scale: 1, min-width: 390, max-width: 390, documentClientWidth: 389', () => {
+    const initialScale = '1'
+    const minWidth = '390'
+    const maxWidth = '390'
+    const viewportContentMap = {
+      width: 'device-width',
+      'initial-scale': initialScale
+    }
+    const viewportExtraContentMap = {
+      'min-width': minWidth,
+      'max-width': maxWidth
+    }
+    const documentClientWidth = 389
+    const contentMap = createAppliedContentMap(
+      viewportContentMap,
+      viewportExtraContentMap,
+      documentClientWidth
+    )
+    expect(contentMap).toStrictEqual({
+      width: minWidth,
+      'initial-scale': `${(documentClientWidth / +minWidth) * +initialScale}`
+    })
+  })
+
+  test('createAppliedContentMap with following params. initial-scale: 1, min-width: 390, max-width: 390, documentClientWidth: 390', () => {
+    const initialScale = '1'
+    const minWidth = '390'
+    const maxWidth = '390'
+    const viewportContentMap = {
+      width: 'device-width',
+      'initial-scale': initialScale
+    }
+    const viewportExtraContentMap = {
+      'min-width': minWidth,
+      'max-width': maxWidth
+    }
+    const documentClientWidth = 390
+    const contentMap = createAppliedContentMap(
+      viewportContentMap,
+      viewportExtraContentMap,
+      documentClientWidth
+    )
+    expect(contentMap).toStrictEqual(viewportContentMap)
+  })
+
+  test('createAppliedContentMap with following params. initial-scale: 1, min-width: 390, max-width: 390, documentClientWidth: 391', () => {
+    const initialScale = '1'
+    const minWidth = '390'
+    const maxWidth = '390'
+    const viewportContentMap = {
+      width: 'device-width',
+      'initial-scale': initialScale
+    }
+    const viewportExtraContentMap = {
+      'min-width': minWidth,
+      'max-width': maxWidth
+    }
+    const documentClientWidth = 391
+    const contentMap = createAppliedContentMap(
+      viewportContentMap,
+      viewportExtraContentMap,
+      documentClientWidth
+    )
+    expect(contentMap).toStrictEqual({
+      width: maxWidth,
+      'initial-scale': `${(documentClientWidth / +maxWidth) * +initialScale}`
+    })
+  })
+
   test('stringify', () => {
     const contentString = stringify({
       width: 'device-width',
       'initial-width': '1'
     })
     expect(contentString).toBe('width=device-width,initial-width=1')
-  })
-
-  test('validateExtraProperties returns true when receiving min-width and max-width, and they are numeric strings and min-width <= max-width', () => {
-    const viewportExtraContentMapValid = validateExtraProperties({
-      'min-width': '375',
-      'max-width': '414'
-    })
-    expect(viewportExtraContentMapValid).toBe(true)
-  })
-
-  test('validateExtraProperties returns false when receiving min-width and max-width, but min-width > max-width', () => {
-    const viewportExtraContentMapValid = validateExtraProperties({
-      'min-width': '414',
-      'max-width': '375'
-    })
-    expect(viewportExtraContentMapValid).toBe(false)
-  })
-
-  test('validateExtraProperties returns false when receiving min-width and max-width, but min-width is not numeric string', () => {
-    const viewportExtraContentMapValid = validateExtraProperties({
-      'min-width': 'foobar',
-      'max-width': '414'
-    })
-    expect(viewportExtraContentMapValid).toBe(false)
-  })
-
-  test('validateExtraProperties returns false when receiving min-width and max-width, but max-width is not numeric string', () => {
-    const viewportExtraContentMapValid = validateExtraProperties({
-      'min-width': '375',
-      'max-width': 'foobar'
-    })
-    expect(viewportExtraContentMapValid).toBe(false)
-  })
-
-  test('validateExtraProperties returns true when receiving min-width, and it is numeric string', () => {
-    const viewportExtraContentMapValid = validateExtraProperties({
-      'min-width': '375'
-    })
-    expect(viewportExtraContentMapValid).toBe(true)
-  })
-
-  test('validateExtraProperties returns false when receiving min-width, and it is not numeric string', () => {
-    const viewportExtraContentMapValid = validateExtraProperties({
-      'min-width': 'foobar'
-    })
-    expect(viewportExtraContentMapValid).toBe(false)
-  })
-
-  test('validateExtraProperties returns true when receiving max-width, and it is numeric string', () => {
-    const viewportExtraContentMapValid = validateExtraProperties({
-      'max-width': '414'
-    })
-    expect(viewportExtraContentMapValid).toBe(true)
-  })
-
-  test('validateExtraProperties returns false when receiving max-width, and it is not numeric string', () => {
-    const viewportExtraContentMapValid = validateExtraProperties({
-      'max-width': 'foobar'
-    })
-    expect(viewportExtraContentMapValid).toBe(false)
-  })
-
-  test('validateExtraProperties returns false when not receiving both of min-width and max-width', () => {
-    const viewportExtraContentMapValid = validateExtraProperties({})
-    expect(viewportExtraContentMapValid).toBe(false)
   })
 })
--- src/lib/ContentMap.ts
+++ src/lib/ContentMap.ts
@@ -1,29 +1,79 @@
 import { ContentString } from './ContentString'
+import {
+  NonnumericInitialScaleError,
+  NoOptionsError,
+  NonnumericMinWidthError,
+  NonnumericMaxWidthError,
+  ReversedOptionsError
+} from './Error'
 
 export interface ContentMap {
   [key: string]: string
+}
+
+export const defaultViewportProps = { 'initial-scale': '1' }
+export const forcedViewportProps = { width: 'device-width' }
+export const defaultViewportExtraProps = {}
+export const forcedViewportExtraProps = {}
+
+export const initializeViewportProps = (
+  contentMap: ContentMap
+): ContentMap => ({
+  ...defaultViewportProps,
+  ...contentMap,
+  ...forcedViewportProps
+})
+
+export const initializeViewportExtraProps = (
+  contentMap: ContentMap
+): ContentMap => ({
+  ...defaultViewportExtraProps,
+  ...contentMap,
+  ...forcedViewportExtraProps
+})
+
+export const createAppliedContentMap = (
+  viewportContentMap: ContentMap,
+  viewportExtraContentMap: ContentMap,
+  documentClientWidth: number
+): ContentMap => {
+  const initialScaleNumber = +viewportContentMap['initial-scale']
+  // Nonnumeric string
+  if (Number.isNaN(initialScaleNumber)) throw new NonnumericInitialScaleError()
+
+  const minWidthDefined = viewportExtraContentMap['min-width'] != null
+  const maxWidthDefined = viewportExtraContentMap['max-width'] != null
+  // No options
+  if (!minWidthDefined && !maxWidthDefined) throw new NoOptionsError()
+
+  const minWidthNumber = +viewportExtraContentMap['min-width']
+  const maxWidthNumber = +viewportExtraContentMap['max-width']
+  // Nonnumeric string
+  if (minWidthDefined && Number.isNaN(minWidthNumber))
+    throw new NonnumericMinWidthError()
+  if (maxWidthDefined && Number.isNaN(maxWidthNumber))
+    throw new NonnumericMaxWidthError()
+  // mix-width > max-width
+  if (minWidthNumber > maxWidthNumber) throw new ReversedOptionsError()
+
+  // Try to apply min-width or max-width to width
+  const contentMap = { ...viewportContentMap }
+  if (minWidthDefined && documentClientWidth < minWidthNumber) {
+    contentMap.width = `${minWidthNumber}`
+    contentMap['initial-scale'] = `${
+      (documentClientWidth / minWidthNumber) * initialScaleNumber
+    }`
+  } else if (maxWidthDefined && documentClientWidth > maxWidthNumber) {
+    contentMap.width = `${maxWidthNumber}`
+    contentMap['initial-scale'] = `${
+      (documentClientWidth / maxWidthNumber) * initialScaleNumber
+    }`
+  }
+  return contentMap
 }
 
 export const stringify = (contentMap: ContentMap): ContentString => {
   return Object.entries(contentMap)
     .map(([key, value]) => `${key}=${value}`)
     .join(',')
-}
-
-export const validateExtraProperties = (contentMap: ContentMap): boolean => {
-  // Not numeric string
-  const minWidthDefined = contentMap['min-width'] != null
-  const numericMinWidth = +contentMap['min-width']
-  if (minWidthDefined && Number.isNaN(numericMinWidth)) return false
-  const maxWidthDefined = contentMap['max-width'] != null
-  const numericMaxWidth = +contentMap['max-width']
-  if (maxWidthDefined && Number.isNaN(numericMaxWidth)) return false
-
-  // Must be mix-width <= max-width
-  // At least one of mix-width or max-width
-  return minWidthDefined
-    ? maxWidthDefined
-      ? numericMinWidth <= numericMaxWidth
-      : true
-    : maxWidthDefined
 }
