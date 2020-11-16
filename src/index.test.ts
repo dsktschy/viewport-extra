d1e26ac9f604cd059da66ab160b516679c142f1e
dai <chamestica@gmail.com>
2020-11-17 03:32:10 +0900

    Do initial run by importing package or loading script

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,11 +1,121 @@
-import ViewportExtra, { version } from './index'
-
 describe('about src/index.ts', () => {
-  test('whether to export version property', () => {
-    expect(typeof version === 'string').toBe(true)
+  test('importing as ES module in document that has no viewport meta element', async () => {
+    const documentClientWidth = 320
+    Object.defineProperty(document.documentElement, 'clientWidth', {
+      value: documentClientWidth,
+      writable: true
+    })
+    document.head.innerHTML = ''
+    // Run module again
+    jest.resetModules()
+    await import('./index')
+    const selectedViewportElement = document.head.querySelector(
+      'meta[name="viewport"]'
+    )
+    const viewportContentString = selectedViewportElement?.getAttribute(
+      'content'
+    )
+    expect(viewportContentString).toBe('initial-scale=1,width=device-width')
   })
 
-  test('whether to export class containing version property', () => {
-    expect(typeof ViewportExtra.version === 'string').toBe(true)
+  test('importing as ES module in document that has <meta name="viewport" content="width=device-width,initial-scale=1" data-extra-content="min-width=375,max-width=414">, and 320px width', async () => {
+    const minWidth = 375
+    const maxWidth = 414
+    const documentClientWidth = 320
+    Object.defineProperty(document.documentElement, 'clientWidth', {
+      value: documentClientWidth,
+      writable: true
+    })
+    document.head.innerHTML = ''
+    const createdViewportElement = document.createElement('meta')
+    createdViewportElement.setAttribute('name', 'viewport')
+    createdViewportElement.setAttribute(
+      'content',
+      'width=device-width,initial-scale=1'
+    )
+    createdViewportElement.setAttribute(
+      'data-extra-content',
+      `min-width=${minWidth},max-width=${maxWidth}`
+    )
+    document.head.appendChild(createdViewportElement)
+    // Run module again
+    jest.resetModules()
+    await import('./index')
+    const selectedViewportElement = document.head.querySelector(
+      'meta[name="viewport"]'
+    )
+    const viewportContentString = selectedViewportElement?.getAttribute(
+      'content'
+    )
+    expect(viewportContentString).toBe(
+      `initial-scale=${documentClientWidth / minWidth},width=${minWidth}`
+    )
+  })
+
+  test('importing as ES module in document that has <meta name="viewport-extra" content="width=device-width,initial-scale=1,min-width=375,max-width=414">, and 320px width', async () => {
+    const minWidth = 375
+    const maxWidth = 414
+    const documentClientWidth = 320
+    Object.defineProperty(document.documentElement, 'clientWidth', {
+      value: documentClientWidth,
+      writable: true
+    })
+    document.head.innerHTML = ''
+    const createdViewportExtraElement = document.createElement('meta')
+    createdViewportExtraElement.setAttribute('name', 'viewport-extra')
+    createdViewportExtraElement.setAttribute(
+      'content',
+      `width=device-width,initial-scale=1,min-width=${minWidth},max-width=${maxWidth}`
+    )
+    document.head.appendChild(createdViewportExtraElement)
+    // Run module again
+    jest.resetModules()
+    await import('./index')
+    const selectedViewportElement = document.head.querySelector(
+      'meta[name="viewport"]'
+    )
+    const viewportContentString = selectedViewportElement?.getAttribute(
+      'content'
+    )
+    expect(viewportContentString).toBe(
+      `initial-scale=${documentClientWidth / minWidth},width=${minWidth}`
+    )
+  })
+
+  test('importing as ES module in document that has <meta name="viewport" content="width=device-width,initial-scale=1">, <meta name="viewport-extra" content="min-width=375,max-width=414">, and 320px width', async () => {
+    const minWidth = 375
+    const maxWidth = 414
+    const documentClientWidth = 320
+    Object.defineProperty(document.documentElement, 'clientWidth', {
+      value: documentClientWidth,
+      writable: true
+    })
+    document.head.innerHTML = ''
+    const createdViewportElement = document.createElement('meta')
+    const createdViewportExtraElement = document.createElement('meta')
+    createdViewportElement.setAttribute('name', 'viewport')
+    createdViewportExtraElement.setAttribute('name', 'viewport-extra')
+    createdViewportElement.setAttribute(
+      'content',
+      'width=device-width,initial-scale=1'
+    )
+    createdViewportExtraElement.setAttribute(
+      'content',
+      `min-width=${minWidth},max-width=${maxWidth}`
+    )
+    document.head.appendChild(createdViewportElement)
+    document.head.appendChild(createdViewportExtraElement)
+    // Run module again
+    jest.resetModules()
+    await import('./index')
+    const selectedViewportElement = document.head.querySelector(
+      'meta[name="viewport"]'
+    )
+    const viewportContentString = selectedViewportElement?.getAttribute(
+      'content'
+    )
+    expect(viewportContentString).toBe(
+      `initial-scale=${documentClientWidth / minWidth},width=${minWidth}`
+    )
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,62 @@
 import * as packageJson from '../package.json'
-import { ensureViewportElement } from './lib/Document'
+import {
+  ensureViewportElement,
+  getViewportExtraElement,
+  getClientWidth
+} from './lib/Document'
+import {
+  ContentMap,
+  initializeViewportProps,
+  initializeViewportExtraProps,
+  applyViewportExtraPropsToViewportProps
+} from './lib/ContentMap'
+import {
+  createViewportContentMap,
+  createViewportExtraContentMap,
+  applyContentMap
+} from './lib/HTMLMetaElement'
 
-if (typeof window !== 'undefined') ensureViewportElement(document)
+let viewportElement: HTMLMetaElement | null = null
+let viewportExtraElement: HTMLMetaElement | null = null
+let viewportContentMap: ContentMap = initializeViewportProps({})
+let viewportExtraContentMap: ContentMap = initializeViewportExtraProps({})
+
+if (typeof window !== 'undefined') {
+  // Keep elements
+  viewportElement = ensureViewportElement(document)
+  viewportExtraElement = getViewportExtraElement(document)
+  // Keep attributes
+  viewportContentMap = {
+    ...viewportContentMap,
+    ...createViewportContentMap(viewportElement)
+  }
+  viewportExtraContentMap = {
+    ...viewportExtraContentMap,
+    ...createViewportExtraContentMap(viewportElement)
+  }
+  if (viewportExtraElement) {
+    viewportContentMap = {
+      ...viewportContentMap,
+      ...createViewportContentMap(viewportExtraElement)
+    }
+    viewportExtraContentMap = {
+      ...viewportExtraContentMap,
+      ...createViewportExtraContentMap(viewportExtraElement)
+    }
+  }
+  // Apply viewportExtraContentMap to viewportContentMap
+  try {
+    viewportContentMap = applyViewportExtraPropsToViewportProps(
+      viewportContentMap,
+      viewportExtraContentMap,
+      getClientWidth(document)
+    )
+  } catch (error) {
+    // Avoid throwing when initial running
+  }
+  // Apply viewportContentMap to viewportElement
+  applyContentMap(viewportElement, viewportContentMap)
+}
 
 export const version = packageJson.version
 
