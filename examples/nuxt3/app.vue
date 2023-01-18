b3b7200e85195e2134f326ba2d38a79450bc436d
dsktschy <chamestica@gmail.com>
2023-01-18 10:18:45 +0900

    Update nuxt3 example

--- examples/nuxt3/app.vue
+++ examples/nuxt3/app.vue
@@ -1,17 +1,34 @@
 <template>
-  <div>
-    <NuxtPage/>
-  </div>
+  <Head>
+    <!-- An ineffective element to disable overriding on page transition -->
+    <!-- Therefore don't set content attribute -->
+    <Meta name="viewport" />
+
+    <!-- Google Fonts -->
+    <Link
+      rel="preconnect"
+      href="https://fonts.googleapis.com"
+    />
+    <Link
+      rel="preconnect"
+      href="https://fonts.gstatic.com"
+      crossOrigin="anonymous"
+    />
+    <Link
+      href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap"
+      rel="stylesheet"
+    />
+  </Head>
+
+  <!-- eslint-disable-next-line vue/no-multiple-template-root -->
+  <NuxtPage/>
 </template>
 
 <script setup>
-import { nextTick } from 'vue'
 import { setContent, updateReference } from 'viewport-extra'
 
 onMounted(() => {
-  nextTick(() => {
-    updateReference()
-    setContent({ minWidth: 414 })
-  })
+  updateReference()
+  setContent({ minWidth: 414 })
 })
 </script>
--- examples/nuxt3/nuxt.config.ts
+++ examples/nuxt3/nuxt.config.ts
@@ -1,16 +1,4 @@
-import { defineNuxtConfig } from 'nuxt'
-
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  meta: {
-    meta: [
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
-    ],
-    link: [
-      { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-      { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: 'anonymous' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Open+Sans&display=swap' }
-    ]
-  },
   css: ['@/assets/css/globals.css']
 })
