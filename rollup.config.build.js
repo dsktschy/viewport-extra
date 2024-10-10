a169e7d6bbebebe5e72a4020ddec72993272229c
dsktschy <chamestica@gmail.com>
2024-10-10 21:25:28 +0900

    chore: update comment for rollup outro

--- rollup.config.build.js
+++ rollup.config.build.js
@@ -16,12 +16,9 @@ const banner =
   ' * Released under the MIT License.\n' +
   ' */'
 
-const outro =
-  // To export class constructor without default key
-  // https://github.com/rollup/rollup/issues/1961#issuecomment-534977678
-  'exports = exports.default;\n' +
-  // To handle with types in test
-  'exports.default = exports;'
+// Enable to use class constructor without default key for iife
+// https://github.com/rollup/rollup/issues/1961#issuecomment-534977678
+const outro = 'exports = exports.default;\n' + 'exports.default = exports;'
 
 // Global variable name for iife
 const name = 'ViewportExtra'
