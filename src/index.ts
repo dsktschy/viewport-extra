181350c636ae79399f9b49b2041b87086f51970c
dsktschy <chamestica@gmail.com>
2025-03-17 22:11:49 +0900

    fix: recover IE11 compatibility

--- src/index.ts
+++ src/index.ts
@@ -22,6 +22,12 @@ import {
 } from './lib/MediaSpecificParameters.js'
 import { createPartialContent } from './lib/number.js'
 
+// Polyfill for Array.from that only has feature to convert NodeList to Array
+Array.from =
+  Array.from ||
+  (<T>(arrayLike: ArrayLike<T>): T[] =>
+    (Array.prototype as T[]).slice.call(arrayLike))
+
 let viewportElement: HTMLMetaElement | null = null
 let viewportExtraElementList: HTMLMetaElement[] = []
 let globalParameters: GlobalParameters | null = null
