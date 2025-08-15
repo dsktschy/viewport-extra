357fa392a2dcfa26d4bf36a09404cb6be8cbb2a0
dsktschy <chamestica@gmail.com>
2025-08-15 11:42:39 +0900

    chore: bump nuxt in examples to v4

--- examples/nuxt/nuxt.config.ts
+++ examples/nuxt/nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  compatibilityDate: '2025-08-15',
   devtools: { enabled: true },
   css: ['@/assets/css/globals.css'],
   app: {
@@ -9,7 +10,7 @@ export default defineNuxtConfig({
         {
           name: 'description',
           content:
-            'This example shows how to use Viewport Extra in a Nuxt.js v3 application.'
+            'This example shows how to use Viewport Extra in a Nuxt application.'
         }
       ]
     }
