3c283142a7c117f1861cfd8c4c55349a7e69f048
dsktschy <chamestica@gmail.com>
2025-11-18 20:39:30 +0900

    chore: fix vulnerabilities

--- examples/next-app-router/next.config.mjs
+++ examples/next-app-router/next.config.mjs
@@ -1,6 +1,10 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  outputFileTracingRoot: path.dirname(fileURLToPath(import.meta.url)),
 };
 
 export default nextConfig;
